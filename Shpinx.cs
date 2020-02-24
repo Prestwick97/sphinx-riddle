142291a819df3ff27880585477ad9dd912db6c6f
Jonathan Carlos, Jieun Kang and patrick delaney <jieun@>
2020-02-24 11:34:59 -0800

    create class and object

--- Shpinx.cs
+++ Shpinx.cs
@@ -1,34 +1,40 @@
 using System;
 using System.Collections.Generic;
 
-class RiddlesOfSphinx
+public class RiddlesOfSphinx
 {
-  static void Main()
+  public string Riddle;
+  public string Answer;
+}
+
+public class Riddle
+{
+  public static void Main()
   {
-    Console.WriteLine("What goes on four feet in the morning, two feet at noon, and three feet in the evening?");
-    string answer = Console.ReadLine();
-    string lowerCaseAnswer = answer.ToLower();
-    if (lowerCaseAnswer == "human" || lowerCaseAnswer == "man")
+    RiddlesOfSphinx one = new RiddlesOfSphinx();
+    one.Riddle = "What goes on four feet in the morning, two feet at noon, and three feet in the evening?";
+    one.Answer = "human";
+
+    RiddlesOfSphinx two = new RiddlesOfSphinx();
+    two.Riddle = "What has hands but no arms and a face but no eyes?";
+    two.Answer = "clock";
+
+    List<RiddlesOfSphinx> Riddles = new List<RiddlesOfSphinx>() { one, two };
+
+    foreach(RiddlesOfSphinx eachRiddle in Riddles)
     {
-      Console.WriteLine("Correct!");
-      Console.WriteLine("What has hands but no arms and a face but no eyes?");
-      string answer2 = Console.ReadLine();
-      string lowerCaseAnswer2 = answer2.ToLower();
-      if (lowerCaseAnswer2 == "clock")
+      Console.WriteLine(eachRiddle.Riddle);
+      string answer = Console.ReadLine();
+      string lowerCaseAnswer = answer.ToLower();
+      if (lowerCaseAnswer == eachRiddle.Answer)
       {
         Console.WriteLine("Correct!");
-        Console.WriteLine("");
       }
-       else 
+      else
       {
-      Console.WriteLine("You lose!");
-      Main();
+        Console.WriteLine("You lose!");
+        Main();
       }
-    }
-    else 
-    {
-      Console.WriteLine("You lose!");
-      Main();
     }
   }
 }
